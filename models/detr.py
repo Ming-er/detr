b87eeb3f04988d7c3657971d1ddc0d2ea893e5ee
Ming-er <47707075+Ming-er@users.noreply.github.com>
2021-12-12 15:43:46 +0800

    Update detr.py

--- models/detr.py
+++ models/detr.py
@@ -36,6 +36,7 @@ class DETR(nn.Module):
         hidden_dim = transformer.d_model
         self.class_embed = nn.Linear(hidden_dim, num_classes + 1)
         self.bbox_embed = MLP(hidden_dim, hidden_dim, 4, 3)
+        # query_embed 代表图像中有多少个目标位置，类似于自学习的 anchor，将这些位置嵌入到 hidden_dim 维度
         self.query_embed = nn.Embedding(num_queries, hidden_dim)
         self.input_proj = nn.Conv2d(backbone.num_channels, hidden_dim, kernel_size=1)
         self.backbone = backbone
